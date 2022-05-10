91c2e4287071e95a2cefcdaecb9c8b789f9d99c5
LuryZhu <zhulury@gmail.com>
2022-05-10 11:13:01 +0800

    add mobile navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,14 @@
 import './style.less'
 import Logo from '../../assets/images/omnifood-logo.png'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  function onClickHandle() {
+    setIsNavOpen(!isNavOpen)
+  }
   return (
-    <header className='header'>
+    <header className={isNavOpen ? 'header nav-open' : 'header'}>
       <img className='logo' src={Logo} alt="omnifood logo" />
       <nav className='main-nav'>
         <ul className='main-nav-list'>
@@ -14,6 +19,15 @@ const Header = () => {
           <li><a className='main-nav-link nav-cta' href="#">Try for free</a></li>
         </ul>
       </nav>
+
+      <button className='btn-mobile-nav' onClick={onClickHandle}>
+        <i className='icon-mobile-nav iconfont icon-menu'
+          name="btn-menu"></i>
+        <i className='icon-mobile-nav iconfont icon-close'
+          name="btn-close"></i>
+      </button>
+
+
     </header>
   )
 }
